973df8ca6ea454ace00611b596b0c009712b0288
nimra-khalid9211 <nimrakhalid502@gmail.com>
2024-06-20 16:48:10 +0500

    testing

--- src/Components/MainComponents/Dashboard.jsx
+++ src/Components/MainComponents/Dashboard.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import DashboardLayout from '../DashboardLayout'
+import { Card1, Card2 } from '../../UI/Cards'
 
 const Dashboard = () => {
   return (
-    <DashboardLayout>Dashboard</DashboardLayout>
+    <DashboardLayout>
+
+<Card1/>
+<Card2/>
+
+    </DashboardLayout>
   )
 }
 
