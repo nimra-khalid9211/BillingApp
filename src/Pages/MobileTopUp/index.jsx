87a4444681fcfcc88589c2680a03c102cc11c031
nimra-khalid9211 <nimrakhalid502@gmail.com>
2024-06-27 11:46:07 +0500

    mobileTopUp finish

--- src/Pages/MobileTopUp/index.jsx
+++ src/Pages/MobileTopUp/index.jsx
@@ -1,12 +1,22 @@
 
+import { Button } from "antd";
 import MobileTopUpTable from "../../Components/Tables/MobileTopUpTable";
 import BreadCrumbs from "../../UI/BreadCrumbs";
+import AddDetails from "./addDetails";
+import { useState } from "react";
 
 export default function MobileTopUp(){
+    const [addNumber, setAddNumber] = useState(false);
     return(
         <>
+        <div className="flex justify-between items-center">
         <BreadCrumbs menu="Mobile Top UP"/>
+        <Button type="primary" shape="round" onClick={() => setAddNumber(true)}>
+         + Add Numbers
+        </Button>
+        </div>
         <MobileTopUpTable/>
+        <AddDetails addNumber={addNumber} setAddNumber={setAddNumber} />
         </>
     )
 }
